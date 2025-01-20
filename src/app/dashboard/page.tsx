95f5deda7a8f4b7b12fce48dd4ea45023bf04534
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-01-19 22:37:14 -0300

    Criando rotas protegidas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
+import { getServerSession } from "next-auth";
 import { Container } from "../../components/container";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
   return (
     <Container>
       <h1>Página dashboard</h1>
