8be59c62202bcb2001d0b7ff76a9cafe3f4acf31
Pollyany Carvalho <kerleypollyany@gmail.com>
2025-01-19 22:52:20 -0300

    Criando tabela tickets

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,47 @@
-import { getServerSession } from "next-auth";
-import { Container } from "../../components/container";
-import { authOptions } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { Container } from '@/components/container'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { redirect } from 'next/navigation'
+import Link from 'next/link'
+import { TicketItem } from '@/app/dashboard/components/ticket'
 
 export default async function Dashboard() {
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions)
 
   if (!session || !session.user) {
-    redirect("/");
+    redirect("/")
   }
+
 
   return (
     <Container>
-      <h1>Página dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text-white">
+            Abrir chamado
+          </Link>
+        </div>
+
+
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-1">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:block">DATA CADASTRO</th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <TicketItem />
+
+            <TicketItem />
+
+          </tbody>
+        </table>
+      </main>
     </Container>
-  );
+  )
 }
